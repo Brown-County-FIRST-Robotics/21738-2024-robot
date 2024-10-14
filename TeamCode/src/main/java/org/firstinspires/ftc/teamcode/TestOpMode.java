584a42195f159caf7a088a00cb0293e9be6da5bb
LinneaPuppy <lkennerhed@gmail.com>
2024-10-14 18:43:03 -0500

    Combined two alternate codebases

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -40,16 +11,20 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-@TeleOp(name="test", group="Iterative OpMode")
-//@Disabled
+@TeleOp(name="Test", group="Iterative OpMode")
 public class BasicOpMode_Iterative extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
+
     private DcMotor frontLeftDrive = null;
     private DcMotor frontRightDrive = null;
     private DcMotor backLeftDrive = null;
     private DcMotor backRightDrive = null;
+
+    private DcMotor arm = null;
+
+    private Servo shoulder = null;
     private Servo elbo = null;
     private Servo hook = null;
 
@@ -68,6 +43,8 @@ public class BasicOpMode_Iterative extends OpMode
         frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
         backLeftDrive  = hardwareMap.get(DcMotor.class, "backLeftDrive");
         backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        shoulder = hardwareMap.get(Servo.class,"shoulder");
         elbo = hardwareMap.get(Servo.class, "elbo");
         hook = hardwareMap.get(Servo.class, "hook");
 
@@ -79,6 +56,7 @@ public class BasicOpMode_Iterative extends OpMode
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -114,16 +92,26 @@ public class BasicOpMode_Iterative extends OpMode
         double rotation = -gamepad1.left_stick_x;
         double movement = -gamepad1.left_stick_y;
         double strafing = -gamepad1.right_stick_x;
+        double uppies = -gamepad2.left_stick_y;
 
         frontLeftDrive.setPower(0.75 * signedSquare(rotation + strafing + movement));
         frontRightDrive.setPower(0.75 * signedSquare(rotation - strafing - movement));
         backLeftDrive.setPower(0.75 * signedSquare(rotation - strafing + movement));
         backRightDrive.setPower(0.75 * signedSquare(rotation + strafing - movement));
 
+        if (uppies > 0.05){
+            arm.setPower(.5);
+        } else if (uppies < -0.05) {
+            arm.setPower(-.5);
+        } else {
+            arm.setPower(0);
+        }
+
+        shoulder.setPosition(shoulder.getPosition() + (gamepad2.left_stick_x * 0.01));
+
+
         if (gamepad2.x) {
             elbo.setPosition(elbo.getPosition() +0.001);
-
-
         }
         if (gamepad2.y) {
             elbo.setPosition(elbo.getPosition() -0.001); //TODO: increase speed
