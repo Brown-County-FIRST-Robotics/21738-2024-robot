354c2c29714c149e7f12d7da4877f9014b79fe40
Huck1111 <askhuckwood@gmail.com>
2024-10-14 16:29:21 -0700

    Alternative code from other laptop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -1,7 +1,35 @@
-
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,37 +38,50 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-
-@TeleOp(name="Emmit Test Program", group="Iterative OpMode")
+@TeleOp(name="test", group="Iterative OpMode")
+//@Disabled
 public class BasicOpMode_Iterative extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    //    private DcMotor frontLeftDrive = null;
-//    private DcMotor frontRightDrive = null;
-//    private DcMotor backRightDrive = null;
-//    private DcMotor backLeftDrive = null;
-    private DcMotor arm = null;
-    private Servo shoulder = null;
+    private DcMotor frontLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor backRightDrive = null;
+    private Servo elbo = null;
+    private Servo hook = null;
 
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
     @Override
-    public void init() { // when you press start, this code runs once
+    public void init() {
         telemetry.addData("Status", "Initialized");
 
-//        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
-//        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
-//        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
-//        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
-        arm = hardwareMap.get(DcMotor.class, "arm");
-        shoulder = hardwareMap.get(Servo.class,"shoulder");
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        frontLeftDrive  = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+        backLeftDrive  = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
+        elbo = hardwareMap.get(Servo.class, "elbo");
+        hook = hardwareMap.get(Servo.class, "hook");
 
 
-//        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-//        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
-//        backRightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-//        backLeftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-        arm.setDirection(DcMotorSimple.Direction.FORWARD);
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
     }
 
     /*
@@ -63,38 +104,37 @@ public class BasicOpMode_Iterative extends OpMode
      */
     @Override
     public void loop() {
-        double rotate = -gamepad1.left_stick_x;
-        double drive = -gamepad1.left_stick_y;
-        double strafe = -gamepad1.right_stick_x;
-        double uppies = -gamepad2.left_stick_y;
-//        double twists = -gamepad2.right_stick_x;
-        // double denominator = Math.max(Math.abs(rotate) + Math.abs(strafe) + Math.abs(drive_), .75);
-        double frontLeftPower = (.75 * .75 * rotate + strafe + drive);// / denominator;
-        double backLeftPower = (.75 * .75 * rotate - strafe + drive);// / denominator;
-        double frontRightPower = (.75 * .75 * rotate - strafe - drive);// / denominator;
-        double backRightPower = (.75 * .75 * rotate + strafe - drive);// / denominator;
-//            frontLeftDrive.setPower(frontLeftPower);
-//            frontRightDrive.setPower(frontRightPower);
-//            backLeftDrive.setPower(backLeftPower);
-//            backRightDrive.setPower(backRightPower);
+        // Setup a variable for each drive wheel to save power level for telemetry
 
-        if (uppies > 0.05){
-            arm.setPower(.5);
-        } else if (uppies < -0.05) {
-            arm.setPower(-.5);
-        } else {
-            arm.setPower(0);
+
+
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+
+        double rotation = -gamepad1.left_stick_x;
+        double movement = -gamepad1.left_stick_y;
+        double strafing = -gamepad1.right_stick_x;
+
+        frontLeftDrive.setPower(0.75 * signedSquare(rotation + strafing + movement));
+        frontRightDrive.setPower(0.75 * signedSquare(rotation - strafing - movement));
+        backLeftDrive.setPower(0.75 * signedSquare(rotation - strafing + movement));
+        backRightDrive.setPower(0.75 * signedSquare(rotation + strafing - movement));
+
+        if (gamepad2.x) {
+            elbo.setPosition(elbo.getPosition() +0.001);
+
+
         }
+        if (gamepad2.y) {
+            elbo.setPosition(elbo.getPosition() -0.001); //TODO: increase speed
+        }
+        telemetry.addData("Position", elbo.getPosition());
 
-        shoulder.setPosition(shoulder.getPosition() + (gamepad2.left_stick_x * 0.01));
-//        if (twists > 0.05){
-//            shoulder.setPosition(shoulder.getPosition()+0.1);
-//        }else if (twists < -0.05){
-//            shoulder.setPosition(shoulder.getPosition()-0.1);
-//        }else {
-//            shoulder.setPosition(0);
-//        }
-
+        if (gamepad2.right_bumper) {
+            hook.setPosition(0.5);}
+        if (gamepad2.left_bumper) {
+            hook.setPosition(0.5); }
+        telemetry.addData("Position2", hook.getPosition());
     }
 
     /*
@@ -102,9 +142,11 @@ public class BasicOpMode_Iterative extends OpMode
      */
     @Override
     public void stop() {
+    }
 
-
-
+    //takes a value and multiplies it by its absolute value, then returns that (square function but keeps the sign)
+    private double signedSquare(double x) {
+        return x * Math.abs(x);
     }
 
 }
